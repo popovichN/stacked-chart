c6c4fee013a9203034aa33e3a07d3ca558b77805
Nadja Popovich <nadja.popovich@nytimes.com>
2017-05-10 11:02:47 -0400

    latest

--- combine.js
+++ combine.js
@@ -7,70 +7,32 @@ var data = JSON.parse(data);
 
 var all_excluded_countries = ['CANADA', 'MEXICO', 'INDIA','JAPAN','CHINA (MAINLAND)','UNITED STATES OF AMERICA','UNITED KINGOM','FRANCE (INCLUDING MONACO)', 'GERMANY', 'FEDERAL REPUBLIC OF GERMANY', 'POLAND', 'ITALY (INCLUDING SAN MARINO)', 'SPAIN', 'BELGIUM', 'CZECHOSLOVAKIA', 'NETHERLANDS', 'ROMANIA', 'AUSTRIA', 'HUNGARY', 'SWEDEN', 'DENMARK', 'BULGARIA', 'GREECE', 'CZECH REPUBLIC', 'FINLAND', 'PORTUGAL', 'IRELAND', 'SLOVAKIA', 'LUXEMBOURG', 'CROATIA', 'ESTONIA', 'SLOVENIA', 'LITHUANIA', 'CYPRUS', 'LATVIA', 'MALTA', 'FORMER GERMAN DEMOCRATIC REPUBLIC'];
 
-//var all_excluded_countries = ['UNITED STATES OF AMERICA', 'CHINA (MAINLAND)'];
-
 var groups = {
-	// 'European Union': ['FRANCE (INCLUDING MONACO)', 'GERMANY', 'FEDERAL REPUBLIC OF GERMANY', 'POLAND', 'ITALY (INCLUDING SAN MARINO)', 'SPAIN', 'BELGIUM', 'CZECHOSLOVAKIA', 'NETHERLANDS', 'ROMANIA', 'AUSTRIA', 'HUNGARY', 'SWEDEN', 'DENMARK', 'BULGARIA', 'GREECE', 'CZECH REPUBLIC', 'FINLAND', 'PORTUGAL', 'IRELAND', 'SLOVAKIA', 'LUXEMBOURG', 'CROATIA', 'ESTONIA', 'SLOVENIA', 'LITHUANIA', 'CYPRUS', 'LATVIA', 'MALTA', 'FORMER GERMAN DEMOCRATIC REPUBLIC'],
-	// 'United Kingdom': [ 'UNITED KINGDOM' ]
-	'United States': ['UNITED STATES OF AMERICA']
-	// 'China': ['CHINA (MAINLAND)'],
-	// 'Japan': ['JAPAN'],
-	// 'India': ['INDIA'],
-	// 'Canada and Mexico': ['CANADA', 'MEXICO']
+	'European Union': ['FRANCE (INCLUDING MONACO)', 'GERMANY', 'FEDERAL REPUBLIC OF GERMANY', 'POLAND', 'ITALY (INCLUDING SAN MARINO)', 'SPAIN', 'BELGIUM', 'CZECHOSLOVAKIA', 'NETHERLANDS', 'ROMANIA', 'AUSTRIA', 'HUNGARY', 'SWEDEN', 'DENMARK', 'BULGARIA', 'GREECE', 'CZECH REPUBLIC', 'FINLAND', 'PORTUGAL', 'IRELAND', 'SLOVAKIA', 'LUXEMBOURG', 'CROATIA', 'ESTONIA', 'SLOVENIA', 'LITHUANIA', 'CYPRUS', 'LATVIA', 'MALTA', 'FORMER GERMAN DEMOCRATIC REPUBLIC'],
+	'United Kingdom': [ 'UNITED KINGDOM' ],
+	'United States': ['UNITED STATES OF AMERICA'],
+	'China': ['CHINA (MAINLAND)'],
+	'Japan': ['JAPAN'],
+	'India': ['INDIA'],
+	'Canada and Mexico': ['CANADA', 'MEXICO']
 };
 
-// var EU = ['FRANCE (INCLUDING MONACO)', 'GERMANY', 'FEDERAL REPUBLIC OF GERMANY', 'POLAND', 'ITALY (INCLUDING SAN MARINO)', 'SPAIN', 'BELGIUM', 'CZECHOSLOVAKIA', 'NETHERLANDS', 'ROMANIA', 'AUSTRIA', 'HUNGARY', 'SWEDEN', 'DENMARK', 'BULGARIA', 'GREECE', 'CZECH REPUBLIC', 'FINLAND', 'PORTUGAL', 'IRELAND', 'SLOVAKIA', 'LUXEMBOURG', 'CROATIA', 'ESTONIA', 'SLOVENIA', 'LITHUANIA', 'CYPRUS', 'LATVIA', 'MALTA', 'FORMER GERMAN DEMOCRATIC REPUBLIC'];
-// var UK = [ 'UNITED KINGOM' ]; 
-// var US = ['UNITED STATES OF AMERICA'];
-// var China = ['CHINA (MAINLAND)'];
-// var Japan = ['JAPAN'];
-// var India = ['INDIA'];
-// var Canada_Mexico = ['CANADA', 'MEXICO'];
-
-// var new_data = [
-// 	{ 
-// 		key: 'United States',
-// 		vlaues: []
-// 	},
-// 	{ 
-// 		key: 'China',
-// 		vlaues: []
-// 	}
-// 	{ 
-// 		key: 'European Union', 
-// 		values: [] 
-// 	},
-// 	{ 
-// 		key: 'United Kingdom',
-// 		vlaues: []
-// 	},
-// 	{ 
-// 		key: 'Japan',
-// 		vlaues: []
-// 	},
-// 	{ 
-// 		key: 'India',
-// 		vlaues: []
-// 	},
-// 	{ 
-// 		key: 'Canada and Mexico',
-// 		vlaues: []
-// 	},
-// 	{ 
-// 		key: 'Rest of world',
-// 		vlaues: []
-// 	}
-
-
-// ];
 var new_data = {
 	'United States': {},
+	'Canada and Mexico' : {},
+	'European Union' : {},
+	'United Kingdom' : {},
+	'Japan' : {},
+	'China' : {},
+	'India' : {},
 	'Rest of world': {}
 };
 
+var rest_of_countries = [];
+
 data.forEach(function (country) {
 
-	//
+	//loop through groups
 	Object.keys(groups).forEach(function (group) {
 
 		var each_group = groups[group];
@@ -86,19 +48,25 @@ data.forEach(function (country) {
 			});
 			
 		} 
-		else if (all_excluded_countries.indexOf(country.key) < 0) {
-			//console.log(country.key )
-			country.values.forEach(function (year_row) {
-				
-				if (!new_data['Rest of world'][year_row.year]){
-					new_data['Rest of world'][year_row.year] = 0;
-				}
-				new_data['Rest of world'][year_row.year] += year_row.emissions;
-			});
-		}
 	});
 
+	//else for rest of world
+	if (all_excluded_countries.indexOf(country.key) < 0) {
+
+		rest_of_countries.push(country.key);
+
+		country.values.forEach(function (year_row) {
+			
+			if (!new_data['Rest of world'][year_row.year]){
+				new_data['Rest of world'][year_row.year] = 0;
+			}
+			new_data['Rest of world'][year_row.year] += year_row.emissions;
+		});
+	}
+
 });
+
+console.log(new_data)
 
 var reformatted_data = [];
 
@@ -118,5 +86,5 @@ Object.keys(new_data).forEach(function (group) {
 	})
 });
 
-fs.writeFileSync('reformatted_data.json', JSON.stringify(reformatted_data), 'utf8')	
+//fs.writeFileSync('reformatted_data.json', JSON.stringify(reformatted_data), 'utf8')	
 })
